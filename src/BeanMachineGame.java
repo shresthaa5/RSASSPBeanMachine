5c86fda7e412fde60e9510530adc0436a39ccdc7
Ruchina Shakya <Ruchina Shakya>
2016-04-13 20:59:46 -0500

    asked for user input

---
+++ src/BeanMachineGame.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author Aditi
+ *
+ */
+// import a scanner
+// Aditi
+import java.util.Scanner;
+public class BeanMachineGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello User!");
+		// call the run method
+			//Ruchina
+
+				// call the test method
+				// Sweksha
+
+
+				System.out.println("That's all folks!");
+
+			}
+	}
